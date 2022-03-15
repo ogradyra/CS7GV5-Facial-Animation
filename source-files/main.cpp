50a2071df755884e7d256b2be228742de2706938
Rachel O'Grady <90624141+ogradyra@users.noreply.github.com>
2022-03-15 14:38:42 +0000

    Update main.cpp

--- source-files/main.cpp
+++ source-files/main.cpp
@@ -372,6 +372,24 @@ void display() {
 	glutSwapBuffers();
 }
 
+void readTextFile(){
+	
+   fstream newfile;
+   newfile.open("tpoint.txt",ios::out);  // open a file to perform write operation using file object
+   if(newfile.is_open())     //checking whether the file is open
+   {
+      newfile<<"Tutorials point \n"; //inserting text
+      newfile.close(); //close the file object
+   }
+   newfile.open("tpoint.txt",ios::in); //open a file to perform read operation using file object
+   if (newfile.is_open()){   //checking whether the file is open
+      string tp;
+      while(getline(newfile, tp)){  //read data from file object and put it into string.
+         cout << tp << "\n";   //print the data of the string
+      }
+      newfile.close();   //close the file object.
+	
+}	
 
 void updateScene() {
 
